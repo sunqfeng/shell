3e3e0d168dc0eb77b9c64f8a79203b7a7ee8edbd
sunqfeng <18722358342@163.com>
2016-10-28 11:37:47 -0400

    my first mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,20 @@
+#########################################################################
+# File Name: mysql.sh
+# Author: sqf
+# mail: 18722358342@163.com
+# Created Time: Thu 20 Oct 2016 10:57:47 AM EDT
+#########################################################################
+#!/bin/bash
+
+i=9;
+for((i;i<10000;i++))
+do
+	MYSQL=`which mysql`
+	name=`date +%s%N | md5sum | head -c 4`
+	age=20+${i};
+	order=106+${i};
+
+	$MYSQL test -u sqf<<!
+	insert into person values(${i},'${name}',${age},${order});
+!
+done
